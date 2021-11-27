9f634d30df9fd32bcdd0129acb5c54179f59c8ad
Darshan Shirke <shirkedarshan@gmail.com>
2021-11-27 13:54:46 +0530

    Contact form

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  log(x: any) {
+    console.log(x);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
